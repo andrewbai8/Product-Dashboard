3b49fb2376ec7e8048a9b471228488aa6c6e6883
andrew <andrewbai@usf.edu>
2026-03-24 14:03:52 -0400

    first commit

---
+++ cp_2.js
@@ -0,0 +1,26 @@
+const API= "https://www.course-api.com/javascript-store-products"
+
+function fetchProductsThen() {
+    fetch(API)
+        .then((response) => response.json())
+            .then((products) => {
+                products.forEach((product) => {
+                    console.log(product.fields.name);
+                });
+            })
+            .catch((error) => {
+                console.error(`Fetch error (then): ${error.message}`);
+
+            });
+        };
+async function fetchProductsAsync() {
+  try {
+    const response = await fetch(API_URL);
+    const products = await response.json();
+    displayProducts(products);
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+
